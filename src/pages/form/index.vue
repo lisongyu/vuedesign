88e7c339ec2225a4629c39933f3e2f326f45e55a
lisongyu <269876940@qq.com>
2021-06-05 22:16:36 +0800

    添加空格

--- src/pages/form/index.vue
+++ src/pages/form/index.vue
@@ -2,35 +2,31 @@
   <div>
     <KForm :model="model">
       <KFormItem label="用户名">
-          <KInput v-model="model.username" placeholder="请输入用户名"></KInput>
+        <KInput v-model="model.username" placeholder="请输入用户名"></KInput>
       </KFormItem>
-        <KFormItem label="密码">
-          <KInput v-model="model.password" placeholder="请输入密码"></KInput>
+      <KFormItem label="密码">
+        <KInput v-model="model.password" placeholder="请输入密码"></KInput>
       </KFormItem>
     </KForm>
-
   </div>
 </template>
-
 <script>
-
-import KInput from './Kinput'
-import KForm from './kForm'
-import KFormItem from './KFormItem'
+import KInput from "./Kinput";
+import KForm from "./kForm";
+import KFormItem from "./KFormItem";
 export default {
-  components:{
+  components: {
     KInput,
     KFormItem,
-    KForm
+    KForm,
   },
-  data(){
+  data() {
     return {
-      model:{
-        username:'tom',
-        password:'12333'
-      }
-    }
-  }
-  
-}
+      model: {
+        username: "tom",
+        password: "12333",
+      },
+    };
+  },
+};
 </script>
